e854c65f7a78510bb39601cae29f091fcc9f8f58
SURE3187774683 <3187774683@qq.com>
2026-05-26 17:17:53 +0800

    Revert "add DeepFollow train and play"
    This reverts commit 172b5fb38468783cf5ef8c33c6cb39302b3ef8ee.

--- source/rl_training/rl_training/tasks/manager_based/locomotion/position/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2025 Deep Robotics
-# SPDX-License-Identifier: BSD 3-Clause
-
-from .cenet_actor_critic import FutureDecoder, HistoryEncoder
-
-__all__ = ["HistoryEncoder", "FutureDecoder"]
